d437799f27624147fcb7d04b3d059cf5c540e33e
hotokezaka <38815342+hotokezaka@users.noreply.github.com>
2021-04-06 01:27:51 +0900

    Update thermalization.py

--- HeatingRate_alpha/thermalization.py
+++ HeatingRate_alpha/thermalization.py
@@ -31,16 +31,18 @@ def calc_kappa_beta(Etmp):
     Z = 54.
     A = 130.
     Imean =  482.*eV
-    gamma = 1.0+E/(me*c*c)
-    tmp3 = 1.0-1.0/gamma/gamma
+    gamma = 1.0 + E/(me*c*c)
+    tmp3 = 1.0 - 1.0/gamma/gamma
     beta = np.sqrt(tmp3)
     tmp1 = gamma*gamma*me*beta*beta*c*c*E/Imean/Imean/2.
     tmp2 = 1./gamma/gamma
-    tmp = np.log(tmp1)-(2./gamma-1.+beta*beta)*np.log(2)+1.-beta*beta+(1.-1./gamma)*(1.-1./gamma)/8.
+    tmp = np.log(tmp1) - (2./gamma-1.+beta*beta)*np.log(2) + 1. - beta*beta + (1.-1./gamma)*(1.-1./gamma)/8.
     omegap = np.sqrt(4.*np.pi*1.0e5*e*e/me)
     tmp_f = np.log(1.123*me*c*c*c*beta*beta*beta/(e*e*omegap))
-
-    return beta*2.*np.pi*Z*np.power(e,4.)*tmp/(me*c*c*beta*beta)/MeV/(A*mu)+ beta*4.*np.pi*np.power(e,4.)*tmp_f/(me*c*c*beta*beta)/MeV/(A*mu)
+    kappabeta = beta*2.*np.pi*Z*np.power(e,4.)*tmp/(me*c*c*beta*beta)/MeV/(A*mu)
+                + beta*4.*np.pi*np.power(e,4.)*tmp_f/(me*c*c*beta*beta)/MeV/(A*mu)
+    
+    return kappabeta 
 
 def calc_kappa_beta_alpha(Etmp):
 #    x = E/0.8
@@ -53,8 +55,8 @@ def calc_kappa_beta_alpha(Etmp):
     A = 130.
     chi = 2.
     Imean =  482.*eV
-    gamma = 1.0+E/(ma*c*c)
-    tmp3 = 1.0-1.0/gamma/gamma
+    gamma = 1.0 + E/(ma*c*c)
+    tmp3 = 1.0 - 1.0/gamma/gamma
     beta = np.sqrt(tmp3)
     tmp1 = gamma*gamma*me*beta*beta*c*c*E/Imean/Imean/2.
     tmp2 = 1./gamma/gamma 
@@ -68,7 +70,7 @@ def calc_kappa_beta_alpha(Etmp):
 #alpha = 0.154157897023##n=9, vmax=0.4c, v0 = 0.1c 
 alpha = 0.0895299932779 #n=5, vmax=0.4c, v0 = 0.2c
 
-def calc_thermalization_time(E0,Mej,vej,Aave,alpha_max,alpha_min,n):
+def calc_thermalization_time(E0, Mej, vej, Aave, alpha_max, alpha_min, n):
     vmax = alpha_max*vej
     vmin = alpha_min*vej
     kappa_beta = calc_kappa_beta(E0)
@@ -85,7 +87,7 @@ def calc_thermalization_time(E0,Mej,vej,Aave,alpha_max,alpha_min,n):
 
 
 
-def calc_thermalization_time_alpha(E0,Mej,vej,Aave,alpha_max,alpha_min,n):
+def calc_thermalization_time_alpha(E0, Mej, vej, Aave, alpha_max, alpha_min, n):
     vmax = alpha_max*vej
     vmin = alpha_min*vej
     kappa_beta_alpha = calc_kappa_beta_alpha(E0)
@@ -104,20 +106,20 @@ def calc_density(Mej,vej,t,alpha_max,alpha_min,n):
     
     return tmp*Mej/(4.0*np.pi*np.power(r0,3.))
 
-def calc_zero_energy(tau0,tau1,E0):
+def calc_zero_energy(tau0, tau1, E0):
     gamma_ad = calc_ad(E0)
     x_ad = 3.*(gamma_ad-1.)
     tmp = (1.0+gam_t)/(2.-(gam_t+1.)*x_ad)
-    tmpp = -2.0+(1.+gam_t)*x_ad
+    tmpp = -2.0 + (1.+gam_t)*x_ad
     x = tau1/tau0
-    return 1.0+tmp*(np.power(x,tmpp)-1.0)/(tau0*tau0)
+    return 1.0 + tmp*(np.power(x,tmpp)-1.0)/(tau0*tau0)
 
-def calc_zero_energy_alpha(tau0,tau1):
+def calc_zero_energy_alpha(tau0, tau1):
     x_ad = 2.
     tmp = (1.0+gam_t_alpha)/(2.-(gam_t_alpha+1.)*x_ad)
-    tmpp = -2.0+(1.+gam_t_alpha)*x_ad
+    tmpp = -2.0 + (1.+gam_t_alpha)*x_ad
     x = tau1/tau0
-    return 1.0+tmp*(np.power(x,tmpp)-1.0)/(tau0*tau0)   
+    return 1.0 + tmp*(np.power(x,tmpp)-1.0)/(tau0*tau0)   
 
 #atau1 = x_ad
 #ntau = -gam_t
@@ -128,12 +130,12 @@ def epsilon_tau(tau0,tau,E0):
     gamma_ad = calc_ad(E0)
     atau1 = 3.*(gamma_ad - 1.)
     ntau = -gam_t
-    pp = -2.+atau1*(1.-ntau)
+    pp = -2. + atau1*(1.-ntau)
     ppp = 1./(1.-ntau)
     tmp = np.power(tau0,-atau1*(1.-ntau))*(1.-ntau)/(atau1*(-ntau+1.)-2.)*(np.power(tau,pp)-np.power(tau0,pp))
-    tmpp =  np.power(x,-atau1*(1.-ntau))*(1. - tmp)
+    tmpp =  np.power(x,-atau1*(1.-ntau))*(1.-tmp)
 
-    if(tmpp>0.):
+    if(tmpp > 0.):
         e_tmp =  np.power(tmpp,ppp)
         if(e_tmp > 0.01):
             return np.power(e_tmp,ntau) #note gam_t is chosen to be constant
@@ -145,14 +147,14 @@ def epsilon_tau(tau0,tau,E0):
 def epsilon_tau_alpha(tau0,tau,E0):
     x = tau/tau0
     gamma_ad = 5./3.
-    atau1 = 3.*(gamma_ad - 1.)
+    atau1 = 3.*(gamma_ad-1.)
     ntau = -gam_t_alpha
     pp = -2.+atau1*(1.-ntau)
     ppp = 1./(1.-ntau)
     tmp = np.power(tau0,-atau1*(1.-ntau))*(1.-ntau)/(atau1*(-ntau+1.)-2.)*(np.power(tau,pp)-np.power(tau0,pp))
     tmpp =  np.power(x,-atau1*(1.-ntau))*(1. - tmp)
 
-    if(tmpp>0.):
+    if(tmpp > 0.):
         e_tmp =  np.power(tmpp,ppp)
         if(e_tmp > 0.01):
             return np.power(e_tmp,ntau) #note gam_t is chosen to be constant
@@ -162,10 +164,10 @@ def epsilon_tau_alpha(tau0,tau,E0):
         return 0.
 
 
-def calc_gamma_deposition(kappa_eff,t,Mej,vej,alpha_min,alpha_max,n):
+def calc_gamma_deposition(kappa_eff, t, Mej, vej, alpha_min, alpha_max, n):
     w = alpha_min/alpha_max
-    k = n-3.0
-    alpha_gam = 0.1*w+0.003*k/w
+    k = n - 3.0
+    alpha_gam = 0.1*w + 0.003*k/w
     t0 = np.sqrt(alpha_gam*kappa_eff*Mej/(vej*vej))
    # print t0/day
     return 1.0 - np.exp(-t0*t0/(t*t))
